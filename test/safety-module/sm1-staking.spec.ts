9305c1e6280c4bed1750749d87b55e04277d3076
Lucas <lucas@dydx.exchange>
2021-12-19 21:14:27 -0800

    Revert changes to sm1-staking

--- test/safety-module/sm1-staking.spec.ts
+++ test/safety-module/sm1-staking.spec.ts
@@ -1,3 +1,6 @@
+import { AFFECTED_STAKERS } from '../../src/lib/affected-stakers';
+import { impersonateAccount } from '../../src/migrations/helpers/impersonate-account';
+import { getAffectedStakersForTest } from '../helpers/get-affected-stakers-for-test';
 import { addStakingTestCases } from './test-cases/staking-test-cases';
 
 describe('SM1Staking with new stakers', () => {
@@ -7,4 +10,15 @@ describe('SM1Staking with new stakers', () => {
       ctx.users[1],
     ],
   );
+});
+
+describe('SM1Staking with stakers affected by the Safety Module bug', () => {
+  if (getAffectedStakersForTest().length >= 2) {
+    addStakingTestCases(
+      async () => [
+        await impersonateAccount(AFFECTED_STAKERS[0]),
+        await impersonateAccount(AFFECTED_STAKERS[1]),
+      ],
+    );
+  }
 });
